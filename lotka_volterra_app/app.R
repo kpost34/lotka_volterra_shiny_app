c5d79faafce8e01848cd67ef0abb92b929f739f4
kpost34 <kpost342005@hotmail.com>
2022-02-27 23:04:26 -0700

    Now app can generate logistic growth

--- lotka_volterra_app/app.R
+++ lotka_volterra_app/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(tidyverse)
 
-
+#Create user interface
 ui<-fluidPage(
   fluidRow(
     column(6,
@@ -17,10 +17,25 @@ ui<-fluidPage(
       sliderInput("per_cap_gr2","r",value=0,min=-1,max=1,step=0.1)
   )),
   fluidRow(
-    column(12,plotOutput("population_growth"))
+    column(6,
+           numericInput("carry1","K",value=100,min=1,max=1000,width="50%")
+    ),
+    column(6,
+           numericInput("carry2","K",value=100,min=1,max=1000,width="50%"))
   ),
+  fluidRow(
+    column(12,
+      "Exponential Growth",
+      plotOutput("exp_growth"))
+  ),
+  fluidRow(
+    column(12,
+      "Logistic Growth",
+      plotOutput("log_growth"))
+  )
 )
 
+#Create functions
 #exponential population growth function
 exp_pop_growth<-function(No,r,t){
   #create empty tibble
@@ -41,6 +56,28 @@ exp_pop_growth<-function(No,r,t){
   popDF
 }
 
+
+#logistic population growth function
+log_pop_growth<-function(No,r,t,K){
+  #create empty tibble
+  logDF<-matrix(NA,nrow=t+1,ncol=3)
+  colnames(logDF)<-c("t","N","dNdt")
+  logDF<-as_tibble(logDF)
+  
+  #populate first row and t values
+  logDF$t<-0:t
+  logDF$N[1]<-No
+  logDF$dNdt[1]<-r * ((K - No)/K) * No
+  
+  #populate cells in systematic fashion
+  for(i in 2:t){
+    logDF$N[i]<-logDF$N[i-1] + logDF$dNdt[i-1]
+    logDF$dNdt[i]<-r * ((K - logDF$N[i])/K) * logDF$N[i]
+  }
+  logDF
+}
+
+
 #concatenate population function
 pop_c<-function(pop1,pop2){
   pop1 %>% 
@@ -48,18 +85,35 @@ pop_c<-function(pop1,pop2){
     add_column(pop=c(rep("pop1",nrow(pop1)),rep("pop2",nrow(pop2)))) -> pops
 }
 
-server<-function(input,output,session){
-  pop_data1<-reactive(exp_pop_growth(No=input$pop1,r=input$per_cap_gr1,t=input$time1))
-  pop_data2<-reactive(exp_pop_growth(No=input$pop2,r=input$per_cap_gr2,t=input$time2))
 
-  output$population_growth<-renderPlot({
-    pop_c(pop_data1(),pop_data2()) %>%
+#Create server
+server<-function(input,output,session){
+  #generate exponential pop growth data
+  exp_data1<-reactive(exp_pop_growth(No=input$pop1,r=input$per_cap_gr1,t=input$time1))
+  exp_data2<-reactive(exp_pop_growth(No=input$pop2,r=input$per_cap_gr2,t=input$time2))
+  
+  #generate logistic pop growth data
+  log_data1<-reactive(log_pop_growth(No=input$pop1,r=input$per_cap_gr1,t=input$time1,K=input$carry1))
+  log_data2<-reactive(log_pop_growth(No=input$pop2,r=input$per_cap_gr2,t=input$time2,K=input$carry2))
+
+  #produce exponential growth plot
+  output$exp_growth<-renderPlot({
+    pop_c(exp_data1(),exp_data2()) %>%
       ggplot() +
       geom_line(aes(t,N,color=pop)) +
       theme_bw() +
       labs(x="time (t)",y="population size (N)",color="Population")
-      
   },res=96)
+
+  #produce logistic growth plot
+  output$log_growth<-renderPlot({
+    pop_c(log_data1(),log_data2()) %>%
+      ggplot() +
+      geom_line(aes(t,N,color=pop)) +
+      theme_bw() +
+      labs(x="time (t)",y="population size (N)",color="Population")
+  })
+  
 }
 shinyApp(ui,server)
 
--- pop_growth_code.R
+++ pop_growth_code.R
@@ -1,16 +1,15 @@
 library(tidyverse)
 
-##Exponential population growth-----------------------------
+##Exponential Population Growth-----------------------------
+#set initial conditions
+No<-50
+r<-0.5
+t<-100
+
 #create the df
 popDF<-matrix(NA,nrow=t+1,ncol=3)
 colnames(popDF)<-c("t","N","dNdt")
 popDF<-as_tibble(popDF)
-
-#set t values, initial N values and r
-#initial conditions/parameters
-No<-50
-r<-0.5
-t<-100
 
 #populate first row and t values
 popDF$t<-0:t
@@ -53,3 +52,55 @@ exp_pop_growth<-function(No,r,t){
 
 #graph N vs. t
 exp_pop_growth(100,.3,50) %>% ggplot(aes(t,N)) + geom_line()
+
+
+
+#Logistic Population Growth-------------------------------------
+#set initial conditions
+No<-50
+r<-0.5
+t<-100
+K<-500
+
+#create the df
+logDF<-matrix(NA,nrow=t+1,ncol=3)
+colnames(logDF)<-c("t","N","dNdt")
+logDF<-as_tibble(logDF)
+
+#populate first row and t values
+logDF$t<-0:t
+logDF$N[1]<-No
+logDF$dNdt[1]<-r * ((K - No)/K) * No
+
+#populate cells in systematic fashion
+for(i in 2:t){
+  logDF$N[i]<-logDF$N[i-1] + logDF$dNdt[i-1]
+  logDF$dNdt[i]<-r * ((K - logDF$N[i])/K) * logDF$N[i]
+}
+
+#create function
+log_pop_growth<-function(No,r,t,K){
+  #create empty tibble
+  logDF<-matrix(NA,nrow=t+1,ncol=3)
+  colnames(logDF)<-c("t","N","dNdt")
+  logDF<-as_tibble(logDF)
+  
+  #populate first row and t values
+  logDF$t<-0:t
+  logDF$N[1]<-No
+  logDF$dNdt[1]<-r * ((K - No)/K) * No
+  
+  #populate cells in systematic fashion
+  for(i in 2:t){
+    logDF$N[i]<-logDF$N[i-1] + logDF$dNdt[i-1]
+    logDF$dNdt[i]<-r * ((K - logDF$N[i])/K) * logDF$N[i]
+  }
+  logDF
+}
+
+#graph N vs. t
+log_pop_growth(50,0.5,10,500) %>% ggplot(aes(t,N)) + geom_line()
+
+
+
+
