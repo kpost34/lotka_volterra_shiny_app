7c700cd6f9dabbcd941ad2391d2de42214c491af
kpost34 <kpost342005@hotmail.com>
2022-03-09 23:46:03 -0700

    Added a "mouse event"--clicking on the plots returns the t, N, dN/dt, and population for the nearest point in the closest graph.

--- lotka_volterra_app/app.R
+++ lotka_volterra_app/app.R
@@ -10,15 +10,15 @@ ui<-fluidPage(
     sidebarPanel(
           h5("Population 1"),
           numericInput("pop1","N",value=50,min=1,max=1000,width="50%"),
-          numericInput("time1","t",value=10,min=2,max=500,width="50%"),
-          sliderInput("per_cap_gr1","r",value=0,min=-1,max=1,step=0.1),
+          numericInput("time1","t",value=20,min=2,max=500,width="50%"),
+          sliderInput("per_cap_gr1","r",value=0,min=-1,max=1,step=0.05),
           numericInput("carry1","K",value=100,min=1,max=1000,width="50%"),
             br(),
             br(),
           h5("Population 2"),
           numericInput("pop2","N",value=50,min=1,max=1000,width="50%"),
-          numericInput("time2","t",value=10,min=2,max=500,width="50%"),
-          sliderInput("per_cap_gr2","r",value=0,min=-1,max=1,step=0.1),
+          numericInput("time2","t",value=20,min=2,max=500,width="50%"),
+          sliderInput("per_cap_gr2","r",value=0,min=-1,max=1,step=0.05),
           numericInput("carry2","K",value=100,min=1,max=1000,width="50%"),
             br(),
             br(),
@@ -31,11 +31,13 @@ ui<-fluidPage(
         tabPanel("Plots",
             br(),
           htmlOutput("exp_title"),
-          plotOutput("exp_growth"),
+          plotOutput("exp_growth",click="plot_click_exp"),
+          tableOutput("coord_exp"),
             br(),
             br(),
           htmlOutput("log_title"),
-          plotOutput("log_growth")),
+          plotOutput("log_growth",click="plot_click_log"),
+          tableOutput("coord_log")),
         tabPanel("Information",
             br(),
           p("This app was developed to illustrate the differences between exponential and logistic population growth",
@@ -101,8 +103,6 @@ pop_c<-function(pop1,pop2){
 
 #Create server function
 server<-function(input,output,session){
-  #thematic::thematic_shiny()
-  
   #generate exponential pop growth data
   exp_data1<-reactive(exp_pop_growth(No=input$pop1,r=input$per_cap_gr1,t=input$time1))
   exp_data2<-reactive(exp_pop_growth(No=input$pop2,r=input$per_cap_gr2,t=input$time2))
@@ -129,6 +129,12 @@ server<-function(input,output,session){
   },res=96) 
   })
   
+  #print exponential plot click output
+  output$coord_exp<-renderTable({
+    req(input$plot_click_exp)
+    nearPoints(pop_c(exp_data1(),exp_data2()),input$plot_click_exp,xvar="t",yvar="N",threshold=10)
+  })
+  
   #print logistic growth title
   observeEvent(input$log_button,{
     output$log_title<-renderText(paste("<b>Logistic Growth</b>"))
@@ -146,9 +152,20 @@ server<-function(input,output,session){
         labs(x="time (t)",y="population size (N)",color="Population")
   },res=96) 
   })
+  
+  #print logistic plot click output
+  output$coord_log<-renderTable({
+    req(input$plot_click_log)
+    nearPoints(pop_c(log_data1(),log_data2()),input$plot_click_log,xvar="t",yvar="N",threshold=10)
+  })
+  
 }
 shinyApp(ui,server)
 
 #future changes
 #1) dynamic UI so that K boxes and perhaps pop2 inputs and graphs change on buttons
-#2) enhanced UI to provide background info in a tab
+#2) double click to remove graph (or disappears on second button click--then remove the "Show" part of each button)
+
+#3) option (toggle button) to have line/point graph--when point graph is select, make mouse events available
+#4) return final population size?
+#5) two sections (via headers) in Information tab--background and how to use app
